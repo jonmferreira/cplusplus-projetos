a750646c05df549fc7c99f0b79e0fd820a62dc6a
Jonmferreira <jonmferreira@gmail.com>
2016-11-01 15:01:01 -0300

    Foi reorganizado para que a defesa do projeto ficasse mais simples
    Ainda falta encapsular as classes.
    Signed-off-by: Jonmferreira <jonmferreira@gmail.com>

--- [ AED2 ]/pp1_aed.cpp
+++ [ AED2 ]/pp1_aed.cpp
@@ -1,90 +1,388 @@
 #include<iostream>
 #include<vector>
+#include<climits>
+#include<queue>//tirar, usado pra testar logica
 
-
+#define infinito INT_MAX
 using namespace std;
 
 /*
 tamanho de um grafo representa a sua quantidade de arestas
-
-Ordem de um grafo é o número de vértices.
+Ordem de um grafo sao o numero de vertices.
 */
+
+class Aresta{
+	public:
+		int			vertice;
+		double		peso;
+		Aresta(){}
+		
+		
+		Aresta(int pDest, double pPeso){
+			vertice = pDest;
+			peso = pPeso;
+		}	
+};
+
 
 class Vertice{
 
 	public :
-		vector<int> lst_adj;
+		
+		
+		vector<Aresta>	arestasAdjacentes;
+		
 		
 		Vertice(){
 		}
-		Vertice(int ordem){
-			lst_adj.assign(ordem,0);
-		}
-};
-
-class Grafo{
-	public:
-		vector<Vertice> vertices;
-		int qtdVertices;
-		Grafo(){
 		
-		}
-		Grafo( int n ){
 		
-				qtdVertices = n;
-				vertices.assign( n, Vertice(n));
+		void addVisinho(int visinho, double peso){
+			Aresta		aresta( visinho, peso );
+			arestasAdjacentes.push_back( aresta );
 		}
+	
 		
 		void print(){
-			cout<< "Grafo:\nquantidade de vertices: "<<vertices.size()
-			<<endl<<"Limite da lista de adj do vertice 0: "
-			<< vertices[0].lst_adj.size()<<endl;
+			for(int i=0;i< arestasAdjacentes.size();i++){
+				cout << arestasAdjacentes[i].vertice << " ";
+			}
+		}
+		
+		
+		void limparMemoria(){
+			arestasAdjacentes.clear();
 		}
 };
 
-class StarTrek_PP1_Desfecho {
-	private:
-		static StarTrek_PP1_Desfecho *desenlace;
-		StarTrek_PP1_Desfecho(){
-		}
-		void posExecute(){
-			grafo->print(); 
-		}
-	public:
 
-		int ordem;
-		Grafo *grafo;
+class Grafo{
+	private:
+	
+	double custoTotalDFS;
+	bool fezBuscaDFS; 
+	
+	
+	Grafo() {
+			
+			fezBuscaDFS = false;
+			custoTotalDFS = -1;
+	}
+	
+	
+	void dfs( int origem, vector<bool>& visitado ){
 		
-		/**
-			Exemplo usando um paradigma fodastico.
-			Ele garante que tenha apenas uma única instancia da classe;
-			Veja que no main, na solucao2 chama uma novaInstancia,
-			Mas ele acaba por ter apenas uma.//vide ln 87.	
-		*/
-		static StarTrek_PP1_Desfecho * novaInstancia(){
-			if( StarTrek_PP1_Desfecho::desenlace == NULL){	
-				StarTrek_PP1_Desfecho::desenlace  = new StarTrek_PP1_Desfecho;	
-				return StarTrek_PP1_Desfecho::desenlace;
-			}
-			else return StarTrek_PP1_Desfecho::desenlace;
+		if( not visitado[origem] ){
+			//cout << "rodou um dfs no vertice: "<<origem<< endl;
+			visitado[origem] = true;
+			Vertice planeta = vertices[origem];
+			for( int i=0; i< planeta.arestasAdjacentes.size();i++){
+				Aresta caminho = planeta.arestasAdjacentes[i]; 
+				//cout<< "testando visinho de vertice: "<< caminho.vertice<< " de peso "<< caminho.peso <<endl;
+				if( not visitado[ caminho.vertice ] ){
+					//cout << "		ok! visinho nao visitado!\n		vamos visitar!\n";
+					//cout << "custo acumulado deste sistema ate aqui::"<< custoTotalDFS <<endl;
+					custoTotalDFS += caminho.peso;
+					//cout<<"custo passa a ser : "<< custoTotalDFS<<endl;
+					dfs(caminho.vertice, visitado);
+				}
+			}	
 		}
+		
+	}
+	
+	
+	void dfs(){
+		
+		if ( not fezBuscaDFS ){
+			
+			vector<bool> 	visitados;
+			visitados.assign( qtdVertices + 1 , false );
+			fezBuscaDFS = true;
+			custoTotalDFS=0;
+			//cout <<"\npassei por aqui"<<endl;
+			dfs(1, visitados);
+		}
+	}
+	
+	public:
+		int					qtdVertices;
+		int					qtdArestas;
+		vector< Vertice >	vertices;
+		
+		
+		Grafo( int n , int m ){
+				Grafo();
+				qtdVertices = n;
+				qtdArestas = m;
+				vertices.assign( qtdVertices + 1, Vertice());
+		}
+		
+		
+		void insereAresta( int origem, int destino, double peso ){
+			if( origem < vertices.size() and destino < vertices.size() ){
+				
+				vertices[origem].addVisinho(destino, peso );
+				vertices[destino].addVisinho(origem, peso );
+				fezBuscaDFS = false;
+				//ordena a lista.	
+				
+			}
+			else{
+				cout <<	"Erro!!!\n	Parametros invalidos\n";
+			}
+		}
+		
+		double getCustoDFS(){
+			
+			dfs();
+			return custoTotalDFS;
+			
+		}
+		
+		
+		void print(){
+			for(int i=1;i<vertices.size();i++){
+				cout << i <<": ";
+				vertices [i].print();
+				cout<< "\n------\n";
+			}
+		}
+		
+		
+		void limparMemoria(){
+			for(int i=0 ;i< vertices.size();i++){
+				vertices[i].limparMemoria();
+			}
+			vertices.clear();
+		}
+};
+
+
+class StarTrek_PP1_Desfecho {
+
+	private:
+		
+		int 				posicaoAtual;
+		int 				posicaoDestino;
+		Grafo*				grafoGalaxia;
+		vector<Grafo>		sistemasPlanetarios;
+		vector<int>			sistemasInimigos;
+		double 				custoMenor;
+		
+		
+		void fazExploracao(vector<int>& visitado){
+			custoMenor = 0 ;
+			for( int i = 0; i < visitado.size(); i++){
+				if( visitado[i] == true or i == posicaoDestino){
+					Grafo* sistema = &sistemasPlanetarios[i];
+					//cout << "somando custos:\n sistema a ser analisado: "<< i<<endl;
+					//cout <<"custo ate aqui da galaxia: "<<custoMenor;
+					custoMenor += (sistema->getCustoDFS());
+					//cout << "\n O custo apos analise:: "<< custoMenor<<endl;
+				}
+			}
+		}
+		
+		
+		bool ehAmigo(int sistemaID){
+			
+			bool ehAliado = true;
+			
+			for(int i=0; i < sistemasInimigos.size()  and ehAliado ; i++ ){
+				
+				ehAliado = sistemaID != sistemasInimigos[ i ] and ehAliado;
+			}
+			return ehAliado;
+		}
+		
+		
+		void bfsBFS( vector<double>& distancias, vector<int>& visitado,
+							queue< Aresta >& filap){
+			
+			bool fezexploracao = false;
+			while( not filap.empty() and not fezexploracao ){
+				
+				Aresta 		aresta = filap.front();
+				int 		sistemaAtual = aresta.vertice;
+				filap.pop();
+				if( not visitado[ sistemaAtual ] and ehAmigo(sistemaAtual) ){
+					
+					visitado [ sistemaAtual ] = true;
+					if( sistemaAtual == posicaoDestino){
+						
+						fazExploracao(visitado);
+						fezexploracao = true;
+					}
+					else{
+						
+						Vertice 		u = grafoGalaxia->vertices [ sistemaAtual ];
+						for(int i = 0; i< u.arestasAdjacentes.size(); i++) {
+							
+							Aresta		adj = u.arestasAdjacentes[ i ];
+							int			comprimentoAtual =  distancias[ sistemaAtual ] + 1 ;
+							if( distancias[ adj.vertice ] > comprimentoAtual ){
+								
+								distancias[ adj.vertice ] = comprimentoAtual;
+								filap.push( adj );
+							}
+						}
+					}
+				}
+			}
+		}
+		
+		
+		void bfs(){
+			
+			vector<double>		distancias;
+			vector<int>			visitados; 
+			
+			distancias.assign( grafoGalaxia->qtdVertices + 1, infinito );
+			visitados.assign( grafoGalaxia->qtdVertices + 1, false );
+			distancias[posicaoAtual] = 0;
+			//implementar fila de prioridades::
+			//queue � uma TAD pronta de fila, implemente o nosso!!!
+			queue < Aresta > filap;
+			filap.push( Aresta( posicaoAtual, distancias[ posicaoAtual ] ) );
+			bfsBFS( distancias,visitados, filap );
+		}
+		
+		double getMenorCusto(){
+			bfs();
+			return custoMenor;
+		}
+		
+		void mapearGrafo(Grafo& grafo){
+			int origem, destino;
+			for(int i=1 ; i <= grafo.qtdArestas; i++ ){
+				
+				cin >> origem >> destino;
+				grafo.insereAresta( origem, destino, 0 );
+			}
+		}
+		
+		
+		void mapearGrafoPonderado(Grafo& grafo){
+			int		origem;
+			int 	destino;
+			double 	peso;
+			
+			for(int i=1 ; i <= grafo.qtdArestas; i++ ){	
+				cin >> origem >> destino >> peso;
+				grafo.insereAresta( origem, destino, peso );
+			}
+			
+		}
+		
+				
+		void processaEntrada_Galaxia(){
+			//cout << "Lendo Galaxia\n";
+			int 	numVertices;
+			int		quantidadeArestas;
+			//cout << "Qual a ordem e o tamanho do grafo? ";
+			cin >> numVertices >> quantidadeArestas;
+			
+			grafoGalaxia = new Grafo( numVertices, quantidadeArestas );
+			mapearGrafo(*grafoGalaxia);
+		}
+		
+		
+		void processaEntrada_Inimigos(){
+			
+			//cout << "Quantos inimigos temos? e Quem sao? ";
+			int		nInimigos;
+			cin >> nInimigos;
+			
+			for(int i=0;i<nInimigos;i++){
+				int 	inimigo;
+				cin >> inimigo;
+				sistemasInimigos.push_back(inimigo);
+			}
+		}
+		
+		
+		void processaEntrada_1aEtapa(){
+			
+			processaEntrada_Galaxia();
+			processaEntrada_Inimigos();
+			
+			//cout << "onde voce esta e pra onde quer ir?"<<endl;
+			cin >> posicaoAtual >> posicaoDestino ;
+		}
+		
+		
+		void processaEntrada_2aEtapa(){
+			
+			Grafo grafozero(0,0);
+			
+			sistemasPlanetarios.push_back(grafozero);
+			
+			for(int i=0; i < grafoGalaxia->qtdVertices; i++ ){
+				int 	ordem;
+				int		tamanho;
+				
+				//cout << "sistema "<<i+1 <<":"<< endl;
+				cin >> ordem >> tamanho;
+				
+				Grafo *grafo = new Grafo(ordem, tamanho);
+				mapearGrafoPonderado(*grafo);
+				
+				sistemasPlanetarios.push_back(*grafo);
+			}
+			
+		}
+		
+	public:
+		
+		
+		StarTrek_PP1_Desfecho(){
+			custoMenor= infinito;
+		}
+		
+		
+			
+		void processaEntrada(){
+			
+			processaEntrada_1aEtapa();
+			processaEntrada_2aEtapa();
+		}
+		
+		
+		
+		void posExecute(){
+			//grafoGalaxia->print(); 
+			cout << getMenorCusto() <<endl;
+			limparMemoria();
+		}	
+		
+		
 		void execute(){
-			cout << "qual a ordem do grafo? ";
-			cin>> ordem;
-			grafo = new Grafo(ordem);
+			
+			processaEntrada();
 			posExecute();
 		}
 		
+		
+		void limparMemoria(){
+			
+			for(int i=0; i < sistemasPlanetarios.size() ; i++){
+				
+				sistemasPlanetarios[i].limparMemoria();
+			}
+			grafoGalaxia->limparMemoria();
+			delete grafoGalaxia;
+			sistemasInimigos.clear();
+			sistemasPlanetarios.clear();
+		}
+		
 };
 
-StarTrek_PP1_Desfecho* StarTrek_PP1_Desfecho::desenlace = NULL;
 
 int main(){	
 
-	StarTrek_PP1_Desfecho *solucao = StarTrek_PP1_Desfecho::novaInstancia();
-	StarTrek_PP1_Desfecho *solucao2 = StarTrek_PP1_Desfecho::novaInstancia();
-	solucao->execute();
-	cout<<"hue >> : "<<solucao2->ordem<<endl;
+	StarTrek_PP1_Desfecho solucao ;
+	solucao.execute();
+	
 	return 0;
 }
-/***/
